513f82fbe67d998c042c77443707f166df828f79
IamAnik2003 <165890354+IamAnik2003@users.noreply.github.com>
2025-03-03 21:54:03 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy:{
-      '/api':'https://link-tree-backend1.onrender.com',
-    },
-  },
-  plugins: [react()], // Plugins should be inside the main object
-})
+  plugins: [react()], // Only the React plugin remains
+});
